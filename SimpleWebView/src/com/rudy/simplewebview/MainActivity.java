3b30822b043bef99a2ad9ecafb1ed3b312ac19d4
Alexander Ignacz <alex.ignacz412@gmail.com>
2017-07-28 21:05:02 +0200

    Set --> download option button Add --> appcompat, design libraries

--- SimpleWebView/src/com/rudy/simplewebview/MainActivity.java
+++ SimpleWebView/src/com/rudy/simplewebview/MainActivity.java
@@ -26,9 +26,13 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_download) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void downloadFile(String url) {
+		
 	}
 }
