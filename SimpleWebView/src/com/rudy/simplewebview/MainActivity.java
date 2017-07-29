ebc6cf55783fc0c8d965f9ee3da5ac8b6b97142c
Alexander Ignacz <alex.ignacz412@gmail.com>
2017-07-29 14:40:46 +0200

    Add dialog, main flow.

--- SimpleWebView/src/com/rudy/simplewebview/MainActivity.java
+++ SimpleWebView/src/com/rudy/simplewebview/MainActivity.java
@@ -1,16 +1,40 @@
 package com.rudy.simplewebview;
 
-import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Request;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.button_float);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+        
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://techslides.com/demos/sample-videos/small.mp4");
 	}
 
 	@Override
@@ -27,12 +51,33 @@ public class MainActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_download) {
+			Intent intent = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
+			startActivity(intent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 	
 	public void downloadFile(String url) {
-		
+		DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        Request request = new Request(Uri.parse(url));
+        dm.enqueue(request);
+	}
+	
+	public void showDialog() {
+		AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
+		builderSingle.setTitle("SAMPLE");
+
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		arrayAdapter.add("MP3");
+		arrayAdapter.add("MP4");
+
+		builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                downloadFile("http://api.soundcloud.com/tracks/159723640/stream?client_id=40ccfee680a844780a41fbe23ea89934");
+            }
+        });
+		builderSingle.show();
 	}
 }
